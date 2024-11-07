a0296a5bd9f2e35e3de73aeb1a09fc1904efefc3
Bonagiri Supriya <132325267+supriya1008@users.noreply.github.com>
2024-11-07 19:28:41 +0530

    json2html.js

--- json2html.js
+++ json2html.js
@@ -1,16 +1,20 @@
 export default function json2html(data) {
-    let table = '<table data-user="supriya.bonagiri2240@gmail.com">';
-    table += '<thead><tr><th>Name</th><th>Age</th><th>Gender</th></tr></thead>';
-    table += '<tbody>';
+    let table = <table data-user=\"shilpykotturu@gmail.com\">;
+    table += <thead>;
+    table += <tr><th>Name</th><th>Age</th><th>Gender</th></tr>;
+    table += </thead>;
+    table += <tbody>;
 
-    data.forEach(row => {
-        table += '<tr>';
-        table += `<td>${row.Name}</td>`;
-        table += `<td>${row.Age}</td>`;
-        table += `<td>${row.Gender ? row.Gender : ''}</td>`;
-        table += '</tr>';
+    data.forEach(item => {
+        table += <tr>;
+        table += <td>${item.Name}</td>;
+        table += <td>${item.Age}</td>;
+        table += <td>${item.Gender || ''}</td>;
+        table += </tr>;
     });
 
-    table += '</tbody></table>';
+    table += </tbody>;
+    table += </table>;
+
     return table;
 }
